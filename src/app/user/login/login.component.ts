2ebade445b1ba4d54ad6bc6c535716050808cb8b
Enes Karakas <enes.coding@gmail.com>
2023-04-14 19:26:04 +0300

    s8 started-finished. TemplateVariables, Validation, Handle Err & Submit

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
+  login() {
+    console.log('Login the system');
+    console.log(this.credentials);
+  }
 }
