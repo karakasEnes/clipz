404039875665f3438dac4187c218158c22b95dba
Enes Karakas <enes.coding@gmail.com>
2023-04-17 21:49:32 +0300

    having Schema for docs before adding to collection!

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  usersCollection: AngularFirestoreCollection<IUser>;
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.usersCollection = db.collection('users');
+  }
 
   async createUser(formData: IUser) {
+    if (!formData.password) {
+      throw new Error('Password not provided!');
+    }
+
     await this.auth.createUserWithEmailAndPassword(
       formData.email as string,
       formData.password as string
     );
 
-    await this.db.collection('users').add({
+    await this.usersCollection.add({
       name: formData.name,
       email: formData.email,
       age: formData.age,
