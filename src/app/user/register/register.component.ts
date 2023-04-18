b323e5dd3e712e0b5a2f126739264b1eda6dcaca
Enes Karakas <enes.coding@gmail.com>
2023-04-18 17:02:05 +0300

    match Validator

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,31 +3,38 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl<number | null>(null, [
-      Validators.required,
-      Validators.min(13),
-      Validators.max(120),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
-    ]),
-    confirm_password: new FormControl('', [Validators.required]),
-    phoneNumber: new FormControl('', [
-      Validators.required,
-      Validators.minLength(14),
-      Validators.maxLength(14),
-    ]),
-  });
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      age: new FormControl<number | null>(null, [
+        Validators.required,
+        Validators.min(13),
+        Validators.max(120),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+        ),
+      ]),
+      confirm_password: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(14),
+        Validators.maxLength(14),
+      ]),
+    },
+    [RegisterValidators.match]
+  );
 
   constructor(private auth: AuthService) {}
 
--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,2 +1,16 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
 export class RegisterValidators {
+  static match(formGroup: AbstractControl): ValidationErrors | null {
+    const controlPsw = formGroup.get('password');
+    const controlPswConfirm = formGroup.get('confirm_password');
+
+    if (!controlPsw || !controlPswConfirm) {
+      return { controlNotFound: true };
+    }
+
+    return controlPsw.value === controlPswConfirm.value
+      ? null
+      : { noMatch: true };
+  }
 }
