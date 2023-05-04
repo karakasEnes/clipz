fe98990dce577d46cb44f1ccaaca11bedc876bea
Enes Karakas <enes.coding@gmail.com>
2023-05-04 16:33:48 +0300

    query and path Params

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
 })
-export class ManageComponent {}
+export class ManageComponent {
+  videoOrder = '1';
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params?.['sort'] === '2' ? '2' : '1';
+    });
+  }
+
+  sort(e: Event) {
+    const { value } = e.target as HTMLSelectElement;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value,
+      },
+    });
+  }
+}
