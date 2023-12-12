6c17c22b064c1d12f0a769e832f4d77caa7c30fa
Enes Karakaş <enes.coding@gmail.com>
2023-12-13 00:53:27 +0300

    querying db

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit {
   videoOrder = '1';
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService
+  ) {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params?.['sort'] === '2' ? '2' : '1';
     });
+  }
+
+  ngOnInit(): void {
+    this.clipService.getUserClips().subscribe(console.log);
   }
 
   sort(e: Event) {
