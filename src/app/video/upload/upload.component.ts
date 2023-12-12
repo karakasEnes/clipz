29884fc543141842c2c084b9c240408a7dd3d9fb
Enes Karakaş <enes.coding@gmail.com>
2023-12-12 20:18:02 +0300

    Error and Success Upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { last } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
@@ -18,6 +19,7 @@ export class UploadComponent {
   alertMsg = 'Please wait! Your clip is being uploaded';
   inSubmission = false;
   percentage = 0;
+  showPercentage = false;
 
   titleFC = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -49,6 +51,7 @@ export class UploadComponent {
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! Your clip is being uploaded';
     this.inSubmission = true;
+    this.showPercentage = true;
 
     const uniqueFileName = uuidv4();
     //Angular will understand automatically we want to store in sub-folder.
@@ -58,5 +61,24 @@ export class UploadComponent {
     uploadTask.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
+
+    uploadTask
+      .snapshotChanges()
+      .pipe(last())
+      .subscribe({
+        next: (snapshot) => {
+          this.alertColor = 'green';
+          this.alertMsg = 'Success! Your clip is ready to be share with world!';
+          this.showPercentage = false;
+        },
+
+        error: (error) => {
+          this.alertColor = 'red';
+          this.alertMsg = 'Upload failed! Please try again later!';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.log(error);
+        },
+      });
   }
 }
