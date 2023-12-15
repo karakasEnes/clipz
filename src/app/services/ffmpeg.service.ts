6508248b224421b798995e2f0707f775e893eda8
Enes Karakaş <enes.coding@gmail.com>
2023-12-15 22:09:32 +0300

    Generating Multiple Screenshots

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -27,19 +27,26 @@ export class FfmpegService {
 
     this.ffmpeg.FS('writeFile', file.name, data);
 
-    await this.ffmpeg.run(
-      //input
-      '-i',
-      file.name,
-      //output options
-      '-ss',
-      '00:00:01',
-      '-frames:v',
-      '1',
-      '-filter:v',
-      'scale=510:-1',
-      // output
-      'output_01.png'
-    );
+    const seconds = [1, 2, 3];
+    const commands: string[] = [];
+
+    seconds.forEach((second) => {
+      commands.push(
+        //input
+        '-i',
+        file.name,
+        //output options
+        '-ss',
+        `00:00:0${second}`,
+        '-frames:v',
+        '1',
+        '-filter:v',
+        'scale=510:-1',
+        // output
+        `output_0${second}.png`
+      );
+    });
+
+    await this.ffmpeg.run(...commands);
   }
 }
