366d57253684aa277b66e72a0119433158b254a2
Enes Karakaş <enes.coding@gmail.com>
2023-12-16 13:55:25 +0300

    Handling Scroll Events

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if (bottomOfWindow) {
+      console.log('BUTTOM OF THE WIN');
+    }
+  };
 }
