ab45b924786b72adb46fb9b47572369b67995e5f
Enes Karakaş <enes.coding@gmail.com>
2023-12-16 17:20:40 +0300

    Selecting elements with the ViewChild Decorator

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ClipComponent {
   id = '';
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
 
   constructor(public route: ActivatedRoute) {
     this.route.params.subscribe((params: Params) => {
