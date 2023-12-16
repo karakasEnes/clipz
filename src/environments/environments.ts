78a9fe34cb3ad9977a1351bf180f75237d5f653e
Enes Karakaş <enes.coding@gmail.com>
2023-12-17 02:56:47 +0300

    env adj

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAllOeVXNOj5BRu1BtSCv-QLjjH4Vuy8zM',
+    authDomain: 'angular-clipz-140ba.firebaseapp.com',
+    projectId: 'angular-clipz-140ba',
+    storageBucket: 'angular-clipz-140ba.appspot.com',
+    messagingSenderId: '202964795507',
+    appId: '1:202964795507:web:edafdcca3052ba33296a06',
+    measurementId: 'G-F4PYLCHH0F',
+  },
+};
