d1574dc98298b8854779d0b56ca483e4e95d113e
Enes Karakaş <enes.coding@gmail.com>
2023-12-16 17:55:45 +0300

    Initializing the Video Player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,18 +1,24 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js/dist/video.min';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
 })
-export class ClipComponent {
+export class ClipComponent implements OnInit {
   id = '';
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
+  player?: videojs.Player;
 
   constructor(public route: ActivatedRoute) {
     this.route.params.subscribe((params: Params) => {
       this.id = params?.['id'];
     });
+  }
+
+  ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement);
   }
 }
